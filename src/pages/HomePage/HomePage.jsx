cb039e23ffae136defaa7cc82c7360b6a34f9aba
AndjelaS00 <andjela00stan@gmail.com>
2023-08-19 19:54:28 +0200

    carousel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Flickity from 'react-flickity-component';
+
+import './HomePage.module.css';
+
 
 export default function HomePage(){
+    const flickityOptions = {
+        initialIndex: 1,
+        autoPlay: 1000,
+        contain: true
+      }
+    
+
     return(
         <>
         <Navbar
@@ -12,6 +24,36 @@ export default function HomePage(){
         text3="Cenovnik"
         text4={<Link to="/prijava">Prijavi se</Link>}
         text5={<Link to="/registracija">Registruj se</Link>}/>
+
+
+<div className="home">
+    <h2>Najkvalitetnija usluga ceka na vas</h2>
+    <Flickity
+          className={'carousel'}
+          elementType={'div'}
+          options={flickityOptions}
+          disableImagesLoaded={false}
+          reloadOnUpdate
+          static
+          >
+          <img src="/public/img/1.jpg" alt="" />
+          <img src="/public/img/3.jpg" alt="" />
+          <img src="/public/img/4.jpg" alt="" />
+          <img src="/public/img/5.jpg" alt="" />
+          <img src="/public/img/2.jpg" alt="" />
+          <img src="/public/img/6.jpg" alt="" />
+          <img src="/public/img/7.jpg" alt="" />
+          <img src="/public/img/10.jpg" alt="" />
+          <img src="/public/img/9.jpg" alt="" />
+          <img src="/public/img/8.jpg" alt="" />
+          <img src="/public/img/11.jpg" alt="" />
+          <img src="/public/img/12.jpg" alt="" />
+          <img src="/public/img/13.jpg" alt="" />
+          <img src="/public/img/14.jpg" alt="" />
+          </Flickity>
+</div>
+
+<Footer></Footer>
         </>
     )
 }
