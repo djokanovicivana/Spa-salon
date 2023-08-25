47f980ca4b3ae4039f5d34e6f0182bc10f4c943d
AndjelaS00 <andjela00stan@gmail.com>
2023-08-25 03:18:16 +0200

    Opis usluge page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Section from '../../components/Section/Section';
 import styled from 'styled-components';
 
+
 export default function HomePage(){
-    
+    const navigate = useNavigate();
+
+    const handleViewServicesClick = () => {
+        navigate("usluge");
+    };
+
+    const handleBookingClick = () => {
+        navigate("prijava");
+    };
 
     return(
         <>
@@ -19,54 +28,55 @@ export default function HomePage(){
         text5={<Link to="/registracija">Registruj se</Link>}/>
 
 <Container>
+    
             <Section
                 title="Manikir"
                 description="Nudimo Vam uslugu klasičnog manikira i manikira sa ojačanjem noktiju"
                 image="1.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Masaža"
                 description="Relax i Sportske masaže sa stručnim licima"
                 image="3.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Šminkanje"
                 description="Profesionalno šminkanje za sve prilike"
                 image="12.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Frizure za svačiji ukus"
                 description="Ne postoji frizura koju naša frizerka ne ume da napravi"
                 image="5.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Veliki izbor boja"
                 description="Za usluge manikira i pedikira spremili smo široku paletu nijansi za vas"
                 image="6.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Šišanje + Feniranje"
                 description="Kombinacija ovih usluga po najpovoljnijim cenama u gradu"
                 image="13.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
             <Section
                 title="Prepustite se stručnim licima"
                 description="Opustite se posle stresnog dana uz RELAX masažu i uživajte"
                 image="2.jpg"
-                leftbtn="Pogledaj sve usluge"
-                rightbtn="Zakazivanje"
+                leftbtn={<div onClick={handleViewServicesClick}>Pogledaj sve usluge</div>}
+                rightbtn={<div onClick={handleBookingClick}>Zakazivanje</div>}
             />
         </Container>
 <Footer></Footer>
